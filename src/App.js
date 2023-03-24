cbb06123e4c6bfe87359f570e617b02b76c80a76
claudia.madrid <claudiamadrid95@gmail.com>
2023-03-23 18:04:31 -0600

    New app Search items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Grid } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container columns={12} justifyContent="center" alignItems={"center"}>
+      <Grid item xs= {8} className="search"> 
+        <label>Search: </label>
+        <input type="text" ></input></Grid>
+    </Grid>
+    
   );
 }
 
